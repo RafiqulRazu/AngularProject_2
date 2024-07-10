883c368be76821a11639ee0c57e306ab04f858da
RafiqulRazu <rafiqulislamrazu977@gmail.com>
2024-07-10 18:46:21 +0600

    add

---
+++ angularproject/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { StudentComponent } from './student/student.component';
+import { LocationComponent } from './location/location.component';
+import { CreatelocationComponent } from './createlocation/createlocation.component';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    StudentComponent,
+    LocationComponent,
+    CreatelocationComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule 
+  ],
+  providers: [
+    provideClientHydration(),
+    provideHttpClient(
+      withFetch()
+    )
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ angularproject/src/app/location/location.component.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { LocationService } from './location.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-location',
+  templateUrl: './location.component.html',
+  styleUrl: './location.component.css'
+})
+export class LocationComponent implements OnInit{
+  locations:any;
+  constructor(
+    private locationService: LocationService,
+    private router: Router,
+    private httpClient: HttpClient
+  ){
+
+  }
+  ngOnInit(): void {
+    this.locations=this.locationService.getAllLocation();
+  }
+
+}
